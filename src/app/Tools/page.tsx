50f7f1d2da297cf220cca74bfd6cbb9d52090206
Wira <zmuhamad064@gmail.com>
2026-01-08 10:24:45 +0700

    update marquee

--- src/app/Tools/page.tsx
+++ src/app/Tools/page.tsx
@@ -6,6 +6,7 @@ import Marquee from "./marquee";
 const Tools = () => {
   return (
     <div>
+        {/* 1.Ke kiri */}
       <Marquee repeat={3} pauseOnHover duration={60} className="w-full">
         <div className="flex items-center gap-6">
           {toolsSections.map((t) => (
@@ -29,8 +30,32 @@ const Tools = () => {
           ))}
         </div>
       </Marquee>
-      {/* sebaliknya */}
+      {/* 2. ke kanan */}
       <Marquee repeat={3} pauseOnHover reverse duration={60} className="w-full">
+        <div className="flex items-center gap-6 mt-4">
+          {toolsSections.map((t) => (
+            <Link
+              key={t.name}
+              href={t.kelasURL}
+              className="inline-flex min-w-40 flex-col items-center gap-2 rounded-md border border-gray-200 p-3 shadow-sm bg-white"
+            >
+              <Image
+                src={t.iconURL}
+                alt={t.name}
+                width={48}
+                height={48}
+                className="rounded"
+              />
+              <div className="text-center">
+                <h3 className="text-sm font-medium text-gray-900">{t.name}</h3>
+                <p className="text-xs text-gray-500">{t.description}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </Marquee>
+      {/* 3. kiri */}
+      <Marquee repeat={3} pauseOnHover duration={60} className="w-full">
         <div className="flex items-center gap-6 mt-4">
           {toolsSections.map((t) => (
             <Link
